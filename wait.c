2469aa2e5458537b30d28fd70d8682c9e0b588fd
JsimonH <992922373@qq.com>
2020-11-14 22:55:48 +0800

    process exit

---
+++ wait.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t rpid = fork();
+    if(rpid < 0)
+    {
+        perror("fork");
+        return 0;
+    }
+    else if(rpid == 0)
+    {
+        //child process
+        printf("I am child process\n");
+        sleep(3);
+        exit(65);
+    }
+    else
+    {
+        //father process
+        pid_t status;
+        //pid_t wait(int *stat_loc);
+        wait(&status);
+        if((status & 0x7F) == 0)
+        {
+            printf("exit normally\n");
+            printf("exitcode = %d\n", (status >> 8) & 0xFF);
+        }
+        else
+        {
+            printf("exit abnormally\n");
+            printf("codeflag=%d\n", (status >> 7) & 0x1);
+        }
+        while(1)
+        {
+            sleep(2);
+            printf("I am father process, I am going to exit\n");
+        }
+    }
+    return 0;
+}
