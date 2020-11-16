b4078262038380c000b18decda359ee278457b0f
JsimonH <992922373@qq.com>
2020-11-16 23:34:29 +0800

    Update

--- standardlibraryIO.c
+++ standardlibraryIO.c
@@ -3,18 +3,18 @@
 
 int main()
 {
-    FILE* fp = NULL;
-    fp = fopen("./test.txt", "w+");
+    FILE* fp = fopen("./test.txt", "w+"); //"w+"表示以读写方式打开,文件不存在时则创建
     if(fp == NULL)
     {
-        perror("fopen error");  //打印上一个系统调用接口的使用错误原因
+        perror("fopen error");
         return -1;
     }
 
-    //fseek跳转读写位置 SEEK_SET --从文件起始偏移 SEEK_CUR --从当前读写位置偏移 SEEK_END --从文件末尾偏移
+    //fseek跳转读写位置 
+    //SEEK_SET --从文件开头偏移 SEEK_CUR --从当前读写位置偏移 SEEK_END --从文件末尾偏移
     fseek(fp, 0, SEEK_END); //将文件的读写位置偏移到末尾
     
-    char buf[] = "Hello World!\n";
+    char buf[] = "Hello World!";
     int ret = fwrite(buf, strlen(buf), 1, fp);
     if(ret == 0)
     {
@@ -22,7 +22,7 @@ int main()
         return -1;
     }
    
-    fseek(fp, 0, SEEK_SET);
+    fseek(fp, 0, SEEK_SET); //将文件的读写位置偏移到开头
    
     char tmp[1024] = {0};
     ret = fread(tmp, 1, 1023, fp);  //在块大小为1的情况下不会出现读到数据依然返回0的情况
--- systemIO.c
+++ systemIO.c
@@ -5,25 +5,28 @@
 
 int main()
 {
-    int fd = -1;
-    umask(0); //将当前进程的文件创建权限掩码设置为0
-    fd = open("./test.txt", O_RDWR|O_CREAT,0777);
+    //flags: O_RDONLY 只读打开   O_WRONLY 只写打开  O_RDWR 可读可写 以上三者必选且只能三选一
+    //可选项: O_CREAT 创建文件   O_TRUNC  截断(清空)文件  O_APPEND 以追加方式打开文件
+    int fd = open("./test.txt", O_RDWR|O_CREAT, 0664);
     if(fd < 0)
     {
         perror("open error");
         return -1;
     }
+    
     lseek(fd, 0, SEEK_END);  //读写位置跳转到文件末尾
-    char* ptr = "Hello World!!\n";
+    
+    const char* ptr = "Hello World!";
     int ret = write(fd, ptr, strlen(ptr));
-    if (ret <0)
+    if (ret < 0)
     {
         perror("write error");
         return -1;
     }
-    printf("ret:%d\n", ret);
+    printf("ret: %d\n", ret);
 
-    lseek(fd, 0, SEEK_SET);  //跳转到文件的起始位置 
+    lseek(fd, 0, SEEK_SET);  //读写位置跳转到文件开头
+    
     char buf[1024] = {0};
     ret = read(fd, buf, 1024);
     if(ret < 0)
