b8c2bab5d8eea36b73fe44c0f55472eb919326f9
JsimonH <992922373@qq.com>
2020-11-19 19:47:39 +0800

    shared memory

---
+++ sharedmemory/writeshm.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/shm.h>
+
+#define shm_key 0x12345678
+int main()
+{
+    //1.创建共享内存
+    int shmid = shmget(shm_key, 1024, IPC_CREAT | 0664);
+    if(shmid < 0)
+    {
+        perror("shmget");
+        return 0;
+    }
+
+    //2.将进程附加到共享内存上
+    void* addr = shmat(shmid, NULL, 0); //传递NULL值,默认操作系统为我们分配地址
+    if(!addr)
+    {
+        perror("shmat");
+        return 0;
+    }
+
+    int count = 0;
+    while(1)
+    {
+        count++;
+        snprintf((char*)addr, 1024, "%s-%d", "hello world", count);
+        sleep(2);
+    }
+
+    //3.从共享内存中分离进程
+    shmdt(addr);
+    
+    //4.销毁共享内存
+    shmctl(shmid, IPC_RMID, NULL);
+    return 0;
+}
