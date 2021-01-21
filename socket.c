051b8199059ec68be63049d4f676f9c333532d8e
JsimonH <992922373@qq.com>
2021-01-21 23:31:36 +0800

    socket

---
+++ socket.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    printf("sockfd = %d\n", sockfd);
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr("192.168.80.193");
+    addr.sin_port = htons(22);
+    
+    int ret = bind(sockfd, (struct sockaddr*)&addr, sizeof(addr));
+    while(1)
+    {
+        sleep(1);
+    }
+    return 0;
+}
